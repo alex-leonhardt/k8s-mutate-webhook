02b548a4d3085faf3ef5886423447ca34e2a7b3e
Eduardo Cardoso <dudubet@gmail.com>
2021-05-20 17:33:17 -0300

    Refactor mutate pkg

--- pkg/mutate/mutate.go
+++ pkg/mutate/mutate.go
@@ -1,84 +1,67 @@
-// Package mutate deals with AdmissionReview requests and responses, it takes in the request body and returns a readily converted JSON []byte that can be
-// returned from a http Handler w/o needing to further convert or modify it, it also makes testing Mutate() kind of easy w/o need for a fake http server, etc.
 package mutate
 
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	"strings"
 
-	v1beta1 "k8s.io/api/admission/v1beta1"
+	admissionv1 "k8s.io/api/admission/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// Mutate mutates
-func Mutate(body []byte, verbose bool) ([]byte, error) {
-	if verbose {
-		log.Printf("recv: %s\n", string(body)) // untested section
-	}
+type Input struct {
+	AdmissionRequest *admissionv1.AdmissionRequest
+	PrimaryRegistry string
+	SecondaryRegistry string
+	Verbose bool
+}
 
-	// unmarshal request into AdmissionReview struct
-	admReview := v1beta1.AdmissionReview{}
-	if err := json.Unmarshal(body, &admReview); err != nil {
-		return nil, fmt.Errorf("unmarshaling request failed with %s", err)
-	}
-
-	var err error
+func Mutate(input *Input) (*admissionv1.AdmissionResponse, error) {
 	var pod *corev1.Pod
 
-	responseBody := []byte{}
-	ar := admReview.Request
-	resp := v1beta1.AdmissionResponse{}
+	admissionResponse := &admissionv1.AdmissionResponse{}
 
-	if ar != nil {
+	if input.AdmissionRequest != nil {
 
 		// get the Pod object and unmarshal it into its struct, if we cannot, we might as well stop here
-		if err := json.Unmarshal(ar.Object.Raw, &pod); err != nil {
-			return nil, fmt.Errorf("unable unmarshal pod json object %v", err)
+		if err := json.Unmarshal(input.AdmissionRequest.Object.Raw, &pod); err != nil {
+			return nil, fmt.Errorf("unable to unmarshal pod json object %v", err)
 		}
 		// set response options
-		resp.Allowed = true
-		resp.UID = ar.UID
-		pT := v1beta1.PatchTypeJSONPatch
-		resp.PatchType = &pT // it's annoying that this needs to be a pointer as you cannot give a pointer to a constant?
+		admissionResponse.Allowed = true
+		admissionResponse.UID = input.AdmissionRequest.UID
+		pT := admissionv1.PatchTypeJSONPatch
+		admissionResponse.PatchType = &pT // it's annoying that this needs to be a pointer as you cannot give a pointer to a constant?
 
 		// add some audit annotations, helpful to know why a object was modified, maybe (?)
-		resp.AuditAnnotations = map[string]string{
+		admissionResponse.AuditAnnotations = map[string]string{
 			"mutateme": "yup it did it",
 		}
 
 		// the actual mutation is done by a string in JSONPatch style, i.e. we don't _actually_ modify the object, but
 		// tell K8S how it should modifiy it
-		p := []map[string]string{}
-		for i := range pod.Spec.Containers {
-			patch := map[string]string{
-				"op":    "replace",
-				"path":  fmt.Sprintf("/spec/containers/%d/image", i),
-				"value": "debian",
+		var p []map[string]string
+		for i, container := range pod.Spec.Containers {
+			if strings.HasPrefix(container.Image, input.PrimaryRegistry) {
+				newImage := input.SecondaryRegistry + strings.TrimPrefix(container.Image, input.PrimaryRegistry)
+				patch := map[string]string{
+					"op":    "replace",
+					"path":  fmt.Sprintf("/spec/containers/%d/image", i),
+					"value": newImage,
+				}
+				p = append(p, patch)
 			}
-			p = append(p, patch)
 		}
 		// parse the []map into JSON
-		resp.Patch, err = json.Marshal(p)
+		admissionResponse.Patch, _ = json.Marshal(p)
 
 		// Success, of course ;)
-		resp.Result = &metav1.Status{
+		admissionResponse.Result = &metav1.Status{
 			Status: "Success",
 		}
 
-		admReview.Response = &resp
-		// back into JSON so we can return the finished AdmissionReview w/ Response directly
-		// w/o needing to convert things in the http handler
-		responseBody, err = json.Marshal(admReview)
-		if err != nil {
-			return nil, err // untested section
-		}
 	}
 
-	if verbose {
-		log.Printf("resp: %s\n", string(responseBody)) // untested section
-	}
-
-	return responseBody, nil
+	return admissionResponse, nil
 }
--- pkg/mutate/mutate_test.go
+++ pkg/mutate/mutate_test.go
@@ -1,161 +1,121 @@
 package mutate
 
 import (
-	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
-	v1beta1 "k8s.io/api/admission/v1beta1"
+	admissionv1 "k8s.io/api/admission/v1"
+	authenticationv1 "k8s.io/api/authentication/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 func TestMutatesValidRequest(t *testing.T) {
-	rawJSON := `{
-		"kind": "AdmissionReview",
-		"apiVersion": "admission.k8s.io/v1beta1",
-		"request": {
-			"uid": "7f0b2891-916f-4ed6-b7cd-27bff1815a8c",
-			"kind": {
-				"group": "",
-				"version": "v1",
-				"kind": "Pod"
+	admissionRequest := &admissionv1.AdmissionRequest{
+		UID: "7f0b2891-916f-4ed6-b7cd-27bff1815a8c",
+		Kind: metav1.GroupVersionKind{
+			Group:   "",
+			Version: "v1",
+			Kind:    "Pod",
+		},
+		Resource: metav1.GroupVersionResource{
+			Group:    "",
+			Version:  "v1",
+			Resource: "pods",
+		},
+		RequestKind: &metav1.GroupVersionKind{
+			Group:   "",
+			Version: "v1",
+			Kind:    "Pod",
+		},
+		RequestResource: &metav1.GroupVersionResource{
+			Group:    "",
+			Version:  "v1",
+			Resource: "pods",
+		},
+		Namespace: "yolo",
+		Operation: "CREATE",
+		UserInfo: authenticationv1.UserInfo{
+			Username: "kubernetes-admin",
+			Groups: []string{
+				"system:masters",
+				"system:authenticated",
 			},
-			"resource": {
-				"group": "",
-				"version": "v1",
-				"resource": "pods"
-			},
-			"requestKind": {
-				"group": "",
-				"version": "v1",
-				"kind": "Pod"
-			},
-			"requestResource": {
-				"group": "",
-				"version": "v1",
-				"resource": "pods"
-			},
-			"namespace": "yolo",
-			"operation": "CREATE",
-			"userInfo": {
-				"username": "kubernetes-admin",
-				"groups": [
-					"system:masters",
-					"system:authenticated"
-				]
-			},
-			"object": {
-				"kind": "Pod",
-				"apiVersion": "v1",
-				"metadata": {
-					"name": "c7m",
-					"namespace": "yolo",
-					"creationTimestamp": null,
-					"labels": {
-						"name": "c7m"
-					},
-					"annotations": {
-						"kubectl.kubernetes.io/last-applied-configuration": "{\"apiVersion\":\"v1\",\"kind\":\"Pod\",\"metadata\":{\"annotations\":{},\"labels\":{\"name\":\"c7m\"},\"name\":\"c7m\",\"namespace\":\"yolo\"},\"spec\":{\"containers\":[{\"args\":[\"-c\",\"trap \\\"killall sleep\\\" TERM; trap \\\"kill -9 sleep\\\" KILL; sleep infinity\"],\"command\":[\"/bin/bash\"],\"image\":\"centos:7\",\"name\":\"c7m\"}]}}\n"
-					}
-				},
-				"spec": {
-					"volumes": [
-						{
-							"name": "default-token-5z7xl",
-							"secret": {
-								"secretName": "default-token-5z7xl"
-							}
-						}
-					],
-					"containers": [
-						{
-							"name": "c7m",
-							"image": "centos:7",
-							"command": [
+		},
+		Object: runtime.RawExtension{
+			Raw: []byte(`{
+							"kind": "Pod",
+							"apiVersion": "v1",
+							"metadata": {
+								"name": "c7m",
+								"namespace": "yolo",
+								"creationTimestamp": null,
+								"labels": {
+									"name": "c7m"
+								}
+							},
+							"spec": {
+								"volumes": [],
+								"containers": [
+							{
+								"name": "c7m",
+								"image": "primary.registry/centos:7",
+								"command": [
 								"/bin/bash"
 							],
-							"args": [
+								"args": [
 								"-c",
 								"trap \"killall sleep\" TERM; trap \"kill -9 sleep\" KILL; sleep infinity"
 							],
-							"resources": {},
-							"volumeMounts": [
-								{
-									"name": "default-token-5z7xl",
-									"readOnly": true,
-									"mountPath": "/var/run/secrets/kubernetes.io/serviceaccount"
-								}
+								"resources": {},
+								"volumeMounts": [],
+								"terminationMessagePath": "/dev/termination-log",
+								"terminationMessagePolicy": "File",
+								"imagePullPolicy": "IfNotPresent"
+							}
 							],
-							"terminationMessagePath": "/dev/termination-log",
-							"terminationMessagePolicy": "File",
-							"imagePullPolicy": "IfNotPresent"
-						}
-					],
-					"restartPolicy": "Always",
-					"terminationGracePeriodSeconds": 30,
-					"dnsPolicy": "ClusterFirst",
-					"serviceAccountName": "default",
-					"serviceAccount": "default",
-					"securityContext": {},
-					"schedulerName": "default-scheduler",
-					"tolerations": [
-						{
-							"key": "node.kubernetes.io/not-ready",
-							"operator": "Exists",
-							"effect": "NoExecute",
-							"tolerationSeconds": 300
-						},
-						{
-							"key": "node.kubernetes.io/unreachable",
-							"operator": "Exists",
-							"effect": "NoExecute",
-							"tolerationSeconds": 300
-						}
-					],
-					"priority": 0,
-					"enableServiceLinks": true
-				},
-				"status": {}
-			},
-			"oldObject": null,
-			"dryRun": false,
-			"options": {
-				"kind": "CreateOptions",
-				"apiVersion": "meta.k8s.io/v1"
-			}
-		}
-	}`
-	response, err := Mutate([]byte(rawJSON), false)
-	if err != nil {
-		t.Errorf("failed to mutate AdmissionRequest %s with error %s", string(response), err)
+								"restartPolicy": "Always",
+								"terminationGracePeriodSeconds": 30,
+								"dnsPolicy": "ClusterFirst",
+								"serviceAccountName": "default",
+								"serviceAccount": "default",
+								"securityContext": {},
+								"schedulerName": "default-scheduler",
+								"tolerations": [
+							{
+								"key": "node.kubernetes.io/not-ready",
+								"operator": "Exists",
+								"effect": "NoExecute",
+								"tolerationSeconds": 300
+							},
+							{
+								"key": "node.kubernetes.io/unreachable",
+								"operator": "Exists",
+								"effect": "NoExecute",
+								"tolerationSeconds": 300
+							}
+							],
+								"priority": 0,
+								"enableServiceLinks": true
+							},
+							"status": {}
+						}`),
+		},
+		Options: runtime.RawExtension{
+			Raw: []byte(`{
+							"kind": "CreateOptions",
+							"apiVersion": "meta.k8s.io/v1"
+						}`),
+		},
 	}
+	response, err := Mutate(&Input{
+		AdmissionRequest:  admissionRequest,
+		PrimaryRegistry:   "primary.registry",
+		SecondaryRegistry: "secondary.registry",
+	})
+	assert.NoError(t, err)
 
-	r := v1beta1.AdmissionReview{}
-	err = json.Unmarshal(response, &r)
-	assert.NoError(t, err, "failed to unmarshal with error %s", err)
+	assert.Equal(t, `[{"op":"replace","path":"/spec/containers/0/image","value":"secondary.registry/centos:7"}]`, string(response.Patch))
+	assert.Contains(t, response.AuditAnnotations, "mutateme")
 
-	rr := r.Response
-	assert.Equal(t, `[{"op":"replace","path":"/spec/containers/0/image","value":"debian"}]`, string(rr.Patch))
-	assert.Contains(t, rr.AuditAnnotations, "mutateme")
-
-}
-
-func TestErrorsOnInvalidJson(t *testing.T) {
-	rawJSON := `Wut ?`
-	_, err := Mutate([]byte(rawJSON), false)
-	if err == nil {
-		t.Error("did not fail when sending invalid json")
-	}
-}
-
-func TestErrorsOnInvalidPod(t *testing.T) {
-	rawJSON := `{
-		"request": {
-			"object": 111
-		}
-	}`
-	_, err := Mutate([]byte(rawJSON), false)
-	if err == nil {
-		t.Error("did not fail when sending invalid pod")
-	}
 }
